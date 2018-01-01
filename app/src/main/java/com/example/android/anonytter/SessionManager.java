25cdf47a75bfc937ab8bfdcfa71954cbc64dcc9f
Ritik Gupta <34607539+ritikrox@users.noreply.github.com>
2018-01-01 15:56:28 +0530

    summary

---
+++ app/src/main/java/com/example/android/anonytter/SessionManager.java
@@ -0,0 +1,122 @@
+package com.example.android.anonytter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+
+import java.util.HashMap;
+
+/**
+ * Created by ritik on 12/31/2017.
+ */
+
+public class SessionManager {
+    // Shared Preferences
+    SharedPreferences pref;
+
+    // Editor for Shared preferences
+    Editor editor;
+
+    // Context
+    Context context;
+
+    // Shared pref mode
+    int PRIVATE_MODE = 0;
+
+    // Sharedpref file name
+    private static final String PREF_NAME = "myPref";
+
+    // All Shared Preferences Keys
+    private static final String IS_LOGIN = "IsLoggedIn";
+
+    // Email address (make variable public to access from outside)
+    public static final String KEY_EMAIL = "email";
+
+    // Constructor
+    public SessionManager(Context context){
+        this.context = context;
+        pref = context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+    /**
+     * Create login session
+     * */
+    public void createLoginSession( String email){
+        // Storing login value as TRUE
+        editor.putBoolean(IS_LOGIN, true);
+
+        // Storing email in pref
+        editor.putString(KEY_EMAIL, email);
+
+        // commit changes
+        editor.commit();
+    }
+
+    /**
+     * Check login method wil check user login status
+     * If false it will redirect user to login page
+     * Else won't do anything
+     * */
+    public void checkLogin(){
+        // Check login status
+        if(!this.isLoggedIn()){
+            // user is not logged in redirect him to Login Activity
+            Intent i = new Intent(context, signup.class);
+            // Closing all the Activities
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+            // Add new Flag to start new Activity
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+            // Starting Login Activity
+            context.startActivity(i);
+        }
+
+    }
+
+
+
+    /**
+     * Get stored session data
+     * */
+    public HashMap<String, String> getUserDetails(){
+        HashMap<String, String> user = new HashMap<String, String>();
+
+        // user email id
+        user.put(KEY_EMAIL, pref.getString(KEY_EMAIL, null));
+
+        // return user
+        return user;
+    }
+
+    /**
+     * Clear session details
+     * */
+    public void logoutUser(){
+        // Clearing all data from Shared Preferences
+        editor.clear();
+        editor.commit();
+
+        // After logout redirect user to Loing Activity
+        Intent i = new Intent(context, signup.class);
+        // Closing all the Activities
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        // Add new Flag to start new Activity
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        // Staring Login Activity
+        context.startActivity(i);
+
+    }
+
+    /**
+     * Quick check for login
+     * **/
+    // Get Login State
+    public boolean isLoggedIn(){
+        return pref.getBoolean(IS_LOGIN, false);
+    }
+}
